cb3b498aba5baa5a16bd601bb82009c00686d987
wangwei <862615093@qq.com>
2024-11-29 11:02:34 +0800

    spring: scope bean 作用范围

---
+++ src/main/java/com/ww/spring/scope/sub/F1.java
@@ -0,0 +1,9 @@
+package com.ww.spring.scope.sub;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope("prototype")
+@Component
+public class F1 {
+}
