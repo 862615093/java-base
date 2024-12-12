16b2b6b22f017376daebca8522fd7682c1a6629e
wangwei <862615093@qq.com>
2024-12-12 09:29:12 +0800

    springmvc: 泛型技巧

---
+++ src/main/java/springmvc/genericSkill/BaseDao.java
@@ -0,0 +1,7 @@
+package springmvc.genericSkill;
+
+public class BaseDao<T> {
+    T findOne() {
+        return null;
+    }
+}
---
+++ src/main/java/springmvc/genericSkill/EmployeeDao.java
@@ -0,0 +1,11 @@
+package springmvc.genericSkill;
+
+public class EmployeeDao extends BaseDao {
+}
+
+
+
+
+
+
+
