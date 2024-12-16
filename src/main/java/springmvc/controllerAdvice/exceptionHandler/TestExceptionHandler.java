2c10773b3a5a34b8656f76c32e2d137c6012c534
wangwei <862615093@qq.com>
2024-12-16 09:29:23 +0800

    springmvc: @ControllerAdvice 之 @ExceptionHandler

---
+++ src/main/java/springmvc/controllerAdvice/exceptionHandler/TestExceptionHandler.java
@@ -0,0 +1,63 @@
+package springmvc.controllerAdvice.exceptionHandler;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+
+@Slf4j
+public class TestExceptionHandler {
+
+//    @SneakyThrows
+//    public static void main(String[] args) {
+//        MockHttpServletRequest request = new MockHttpServletRequest();
+//        MockHttpServletResponse response = new MockHttpServletResponse();
+//
+//        //异常解析器
+//        ExceptionHandlerExceptionResolver resolver = new ExceptionHandlerExceptionResolver();
+//        resolver.setMessageConverters(Collections.singletonList(new MappingJackson2XmlHttpMessageConverter()));
+//        resolver.afterPropertiesSet();
+//
+//        HandlerMethod handlerMethod = new HandlerMethod(new Controller1(), Controller1.class.getMethod("foo"));
+//        Exception exception = new Exception("e1");
+//        resolver.resolveException(request, response, handlerMethod, exception); //核心
+//
+//        System.out.println(new String(response.getContentAsByteArray(), StandardCharsets.UTF_8));
+//    }
+
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebConfig.class);
+        ExceptionHandlerExceptionResolver resolver = context.getBean(ExceptionHandlerExceptionResolver.class);
+
+        HandlerMethod handlerMethod = new HandlerMethod(new Controller1(), Controller1.class.getMethod("foo"));
+        Exception exception = new Exception("e1");
+        resolver.resolveException(request, response, handlerMethod, exception);//核心
+
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        System.out.println(new String(response.getContentAsByteArray(), StandardCharsets.UTF_8));
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+    }
+
+
+
+    static class Controller1 {
+        public void foo() {
+        }
+    }
+
+
+}
+
+
---
+++ src/main/java/springmvc/controllerAdvice/exceptionHandler/WebConfig.java
@@ -0,0 +1,36 @@
+package springmvc.controllerAdvice.exceptionHandler;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Configuration
+public class WebConfig {
+
+    @ControllerAdvice
+    static class MyControllerAdvice {
+        @ExceptionHandler
+        @ResponseBody
+        public Map<String, Object> handle(Exception e) {
+            return Collections.singletonMap("error", e.getMessage());
+        }
+    }
+
+    /**
+     * 注入异常解析器
+     */
+    @Bean
+    public ExceptionHandlerExceptionResolver resolver() {
+        ExceptionHandlerExceptionResolver resolver = new ExceptionHandlerExceptionResolver();
+        resolver.setMessageConverters(Collections.singletonList(new MappingJackson2HttpMessageConverter()));
+        // 无需调用 resolver.afterPropertiesSet(); 方法，这是 Spring 的提供的内置拓展，会在 Spring 生命周期中自动执行
+        return resolver;
+    }
+}
